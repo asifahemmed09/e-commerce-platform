ce57b65e97d68da7c9923a9be6f3d4d653d50f61
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-02-03 22:34:25 +0600

    create-best-selling-slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,28 +1,25 @@
-import ProductSlider from '@/components/shared/product/product-slider'
-import { Card, CardContent } from '@/components/ui/card'
+import ProductSlider from '@/components/shared/product/product-slider';
+import { Card, CardContent } from '@/components/ui/card';
 import {
   getAllCategories,
   getProductsByTag,
   getProductsForCard,
-} from '@/lib/actions/product.actions'
-import { HomeCarousel } from '../../components/shared/home/home-carousel'
-import { HomeCard } from '../../components/shared/home/home-card'
-import data from '../../lib/data'
+} from '@/lib/actions/product.actions';
+import { HomeCarousel } from '../../components/shared/home/home-carousel';
+import { HomeCard } from '../../components/shared/home/home-card';
+import data from '../../lib/data';
 
 export default async function HomePage() {
-  const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
-
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' });
   return (
-    <>
-      <HomeCarousel items={data.carousels} />
-      <div className='md:p-4 md:space-y-4 bg-border'>
-        <HomeCard cards={cards} />
-        <Card className='w-full rounded-none'>
-          <CardContent className='p-4 items-center gap-3'>
-            <ProductSlider title={"Today's Deals"} products={todaysDeals} />
-          </CardContent>
-        </Card>
-      </div>
-    </>
-  )
+    <Card className="w-full rounded-none">
+      <CardContent className="p-4 items-center gap-3">
+        <ProductSlider
+          title="Best Selling Products"
+          products={bestSellingProducts}
+          hideDetails
+        />
+      </CardContent>
+    </Card>
+  );
 }
