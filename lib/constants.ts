27d253949bc3a7637663618eacfb33f16fa74e95
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-02-03 22:37:57 +0600

    create-product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,5 @@ export const APP_SLOGAN =
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'Your one-stop online shop for everything you need.'
+
+  export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
