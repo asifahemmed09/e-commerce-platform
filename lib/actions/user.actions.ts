95247f43afe9032815af4255892e29c90c9510bd
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-02-03 23:12:31 +0600

    signin-user

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,11 @@
+'use server'
+import { signIn } from '@/auth'
+import { IUserSignIn } from '@/types'
+
+export async function signInWithCredentials(user: IUserSignIn) {
+  return await signIn('credentials', { ...user, redirect: false })
+}
+export const SignOut = async () => {
+  const redirectTo = await signOut({ redirect: false })
+  redirect(redirectTo.redirect)
+}
